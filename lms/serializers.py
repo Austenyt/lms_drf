9ebb1a6f32f5bd8b19173d677c65953e9b5ec98f
Austenyt <austenyt@gmail.com>
2024-03-12 16:54:47 +0300

    Serializer set

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,9 +4,14 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    num_lessons = serializers.SerializerMethodField()
+
+    def get_num_lessons(self, course):
+        return course.lesson_set.count() if course.lesson_set.exists() else 0
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'name', 'preview', 'description', 'num_lessons']
 
 
 class LessonSerializer(serializers.ModelSerializer):
