fb56d765f673c2bf7068cfeed4e227ab3e9159c6
Austenyt <austenyt@gmail.com>
2024-03-13 17:00:00 +0300

    Serializer set

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
